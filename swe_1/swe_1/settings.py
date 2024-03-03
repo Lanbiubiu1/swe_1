e3e6ff942388163f5567d6cc5385de54b0dba27d
Lanbiubiu1 <95837786+Lanbiubiu1@users.noreply.github.com>
2024-03-02 19:44:17 -0500

    short the line config

--- swe_1/swe_1/settings.py
+++ swe_1/swe_1/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*!62ql9yu_y*y4dnwq+wn&!f25=rv!$5_erpp!ds9qr#2tlaqw'
+SECRET_KEY = ('django-insecure-*!62ql9yu_y*y'
+              '4dnwq+wn&!f25=rv!$5_erpp!ds9qr#2tlaqw')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
