1c87d6781d94fafd0077f4b54d5280eb4c696ae5
Lanbiubiu1 <95837786+Lanbiubiu1@users.noreply.github.com>
2024-03-02 22:56:46 -0500

    30th config

--- swe_1/polls/migrations/0001_initial.py
+++ swe_1/polls/migrations/0001_initial.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -28,14 +27,14 @@ class Migration(migrations.Migration):
             fields=[
                 ('id',
                  models.BigAutoField(auto_created=True, primary_key=True,
-                                           serialize=False, verbose_name='ID')),
+                                     serialize=False, verbose_name='ID')),
                 ('choice_text',
                  models.CharField(max_length=200)),
                 ('votes',
                  models.IntegerField(default=0)),
                 ('question',
                  models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
-                                               to='polls.question')),
+                                   to='polls.question')),
             ],
         ),
     ]
